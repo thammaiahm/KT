aeef575079afe23d1e44f87fff8f94c28ad55f4a
Motorola123 <rviswa@motorola.com>
2015-05-06 14:59:31 +0530

    Added Mail triggering purpose.
    Change-Id: Id4f9bee63f60c38476c5fe58a75597c78d457ea7
    Reviewed-on: http://gerrit.mot.com/744802
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Haruit Kumar <hkumar@motorola.com>

---
+++ src/com/mot/upd/pcba/utils/MailUtil.java
@@ -0,0 +1,96 @@
+/**
+ * 
+ */
+package com.mot.upd.pcba.utils;
+
+import java.util.Properties;
+import java.util.PropertyResourceBundle;
+
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+import org.apache.log4j.Logger;
+/**
+ * @author rviswa
+ *
+ */
+public class MailUtil {
+
+	private static Logger logger = Logger.getLogger(MailUtil.class);
+	private static PropertyResourceBundle bundle = InitProperty
+			.getProperty("pcbaMail.properties");
+
+
+	public static boolean sendEmail()throws Exception{
+
+		boolean isMailSent = false;
+		String email = bundle.getString("email");
+		final String username = bundle.getString("username");
+		final String password = bundle.getString("password");
+		String server=bundle.getString("server");
+		String portNO=bundle.getString("portNO");
+		// Sender's email ID needs to be mentioned
+		String from = bundle.getString("from");
+		String emailSubject=bundle.getString("emailSubject");
+		String emailBody=bundle.getString("emailBody");
+		// Recipient's email ID needs to be mentioned.
+		String to = email;
+		// Get system properties
+		Properties properties = System.getProperties();
+		// Setup mail server
+		properties.setProperty("mail.smtp.auth", "true");
+		properties.setProperty("mail.smtp.starttls.enable", "true");
+		properties.setProperty("mail.smtp.host", server);
+		properties.setProperty("mail.smtp.port", portNO);
+
+		// Get the default Session object.
+		Session session = Session.getInstance(properties,
+				new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
+
+		try{
+			// Create a default MimeMessage object.
+			Multipart mp = new MimeMultipart();
+			MimeMessage message = new MimeMessage(session);
+
+			// Set From: header field of the header.
+			message.setFrom(new InternetAddress(from));
+			message.setSubject(emailSubject);
+			// Set To: header field of the header.
+			message.addRecipient(Message.RecipientType.TO,
+					new InternetAddress(to));
+			BodyPart messageBodyPart = new MimeBodyPart();
+
+			messageBodyPart.setContent(emailBody, "text/html");
+			mp.addBodyPart(messageBodyPart);
+			// Set Subject: header field
+			message.setContent(mp);
+
+
+
+			// Send message
+			Transport.send(message);
+			System.out.println("Sent message successfully....");
+			isMailSent = true;
+		}catch (MessagingException mex) {
+			mex.printStackTrace();
+			throw mex;
+		}
+
+
+		return isMailSent;
+
+	} 	
+}
