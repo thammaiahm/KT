99b33025df879ec526ec7af59f399feddb9842dc
Motorola123 <rviswa@motorola.com>
2015-05-06 19:30:13 +0530

    Modified constants and Queries .
    Change-Id: If4b558a57d523541746d136957cf8c169b3d6834
    Reviewed-on: http://gerrit.mot.com/744957
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Haruit Kumar <hkumar@motorola.com>

--- src/com/mot/upd/pcba/constants/ServiceMessageCodes.java
+++ src/com/mot/upd/pcba/constants/ServiceMessageCodes.java
@@ -21,13 +21,13 @@ public class ServiceMessageCodes {
 	public static final int NO_NEW_SERIAL_NO_AVAILABLE = 5001;
 	public static final int INVALID_REQUEST_TYPE = 5002;
 	public static final int INVALID_BUILD_TYPE = 5003;
-	public static final int SERIAL_NO_NOT_VALID = 5004;
+	public static final int SERIAL_NO_NOT_VALID = 5009;
 	public static final int INPUT_PARAM_MISSING = 5005;
 	public static final int NO_ULMA_AVAILABLE = 5006;
 	public static final int NEW_SERIAL_NO_AVAILABLE = 5007;
 	public static final int NEW_ULMA_AVAILABLE = 5008;
 	public static final int INVALID_SN_TYPE = 5015;
-	public static final int NO_PROTOCOL_FOUND = 5016;
+	public static final int NO_PROTOCOL_FOUND = 5022;
 	public static final int ULMA_ADDRESS_GREATER_THAN_FIVE = 5017;
 	public static final int INVALID_GPPID = 5018;
 
@@ -53,7 +53,7 @@ public class ServiceMessageCodes {
 	public static final String INVALID_REQUEST_TYPE_MSG = "Request type Value must be V or D";
 	public static final String INVALID_BUILD_TYPE_MSG = "Build type Value must be PROD";
 	public static final String SERIAL_NO_NOT_VALID_MSG = "Serial number not valid";
-	public static final String INPUT_PARAM_MISSING_MSG = "Following Fields are mandatory- Serial Number,Build type,Request-type,RSD id,MASC ID.Please re-enter and try again.";
+	public static final String INPUT_PARAM_MISSING_MSG = "Following Fields are mandatory- Build type,Request-type,RSD id,MASC ID.Please re-enter and try again.";
 	public static final String NO_ULMA_AVAILABLE_MSG = "No ULMA Available For Dispatch";
 	public static final String NEW_ULMA_AVAILABLE_MSG = "New ULMA Available For Dispatch";
 	public static final String SERIAL_NO_AVAILABLE_FOR_DISPATCH_MSG = "New Serial number available for Dispatch";
