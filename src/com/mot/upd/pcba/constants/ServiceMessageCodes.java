e155eddd11536bff90a3413c3730ecfe6d1d49f1
Motorola123 <rviswa@motorola.com>
2015-05-08 15:34:18 +0530

    Added constants for PCBA
    Change-Id: I21f1e5562f1613f3d4273aa4f2ec51ed09f016c4
    Reviewed-on: http://gerrit.mot.com/745885
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Haruit Kumar <hkumar@motorola.com>

--- src/com/mot/upd/pcba/constants/ServiceMessageCodes.java
+++ src/com/mot/upd/pcba/constants/ServiceMessageCodes.java
@@ -35,27 +35,26 @@ public class ServiceMessageCodes {
 
 	// Viswanath added
 
-	//public static final int MEID_SUCCESS = 5009;
+	// public static final int MEID_SUCCESS = 5009;
 	public static final int MEID_FAILURE = 5010;
 	public static final int INVALID_STATUS = 5011;
 	public static final int NO_LOCK_CODE_FOUND = 5012;
 	public static final int OLD_SERIAL_NO_NOT_FOUND = 5013;
 	public static final int NEW_SERIAL_NO_NOT_FOUND = 5014;
-	//public static final int IMEI_SUCCESS = 5016;
+	// public static final int IMEI_SUCCESS = 5016;
 	public static final int IMEI_FAILURE = 5017;
 	public static final int OLD_SERIAL_NO_NOT_FOUND_IN_SHIPMENT_TABLE = 5018;
 	public static final int OLD_SERIAL_NO_NOT_FOUND_IN_WARRANTY_INFO_TABLE = 5019;
 	public static final int DUAL_SERIAL_NOT_FOUND = 5020;
-	public static final int TRI_SERIAL_NOT_FOUND=5021;
-	
+	public static final int TRI_SERIAL_NOT_FOUND = 5021;
+
 	// Thammaiah added
 	// Messages for dispatch serial no WS
 	public static final String OPERATION_SUCCESS = "Successfully dispatched serial number";
 	public static final String NO_SERIAL_NO_AVAILABLE_FOR_DISPATCH_MSG = "No New Serial number available for Dispatch";
 	public static final String INVALID_REQUEST_TYPE_MSG = "Request type Value must be V or D";
-	public static final String INVALID_BUILD_TYPE_MSG = "Build type Value must be PROD";
 	public static final String SERIAL_NO_NOT_VALID_MSG = "Serial number not valid";
-	public static final String INPUT_PARAM_MISSING_MSG = "Following Fields are mandatory- Build type,Request-type,RSD id,MASC ID.Please re-enter and try again.";
+	public static final String INPUT_PARAM_MISSING_MSG = "Following Fields are mandatory- Build type,Request-type,SerialNumber Request-type(MEID/IMEI),RSD id,MASC ID,Number of ULMA,Track ID.Please re-enter and try again.";
 	public static final String NO_ULMA_AVAILABLE_MSG = "No ULMA Available For Dispatch";
 	public static final String NEW_ULMA_AVAILABLE_MSG = "New ULMA Available For Dispatch";
 	public static final String SERIAL_NO_AVAILABLE_FOR_DISPATCH_MSG = "New Serial number available for Dispatch";
@@ -63,6 +62,7 @@ public class ServiceMessageCodes {
 	public static final String NO_PROTOCOL_FOUND_MSG = "Protocol Name is mandatory for MEID";
 	public static final String ULMA_ADDRESS_GREATER_THAN_FIVE_MSG = "Ulma address requested is greater than five";
 	public static final String INVALID_GPPID_MSG = "Invalid GPPID";
+	public static final String INVALID_BUILD_TYPE_MSG = "Build type Value must be PROD/PROTO";
 
 	// Viswanath added
 
@@ -72,22 +72,22 @@ public class ServiceMessageCodes {
 	public static final String NO_LOCK_CODE_DETAILS_FOUND_MSG = "No Lock Code Details Found";
 	public static final String OLD_SERIAL_NO_NOT_FOUND_MSG = "Old Serial Not Found";
 	public static final String NEW_SERIAL_NO_NOT_FOUND_MSG = "New Serial Not Found";
-	public static final String NO_DATASOURCE_FOUND_FOR_SERIAL_NO_MSG ="No DataSource found for SerialNO.";
+	public static final String NO_DATASOURCE_FOUND_FOR_SERIAL_NO_MSG = "No DataSource found for SerialNO.";
 	public static final String IMEI_SUCCES_MSG = "IMEI Success";
 	public static final String IMEI_FAILURE_MSG = "IMEI Failure";
 	public static final String PCBA_INPUT_PARAM_MISSING_MSG = "Following Fields are mandatory- Serial Number,rsdID,mascID,status,snType and either one is mandatory msl,otksl and servicePassCode.Please re-enter and try again.";
 	public static final String PCBA_INPUT_PARAM_MISSING = "Following Fields are mandatory- ClientIP,mascID,serialNoIn,serialNoOut,serialNoType,repairdate.Please re-enter and try again.";
-	public static final String OLD_SERIAL_NO_NOT_FOUND_IN_SHIPMENT_TABLE_MSG ="Old Serialno not found in Shipment table";
-	public static final String READING_OLD_SERIAL_NO_INTO_NEW_SERIAL_NO ="Reading old serial number data and Inserted into new Serial Number in Shipment table";
-	public static final String  OLD_SERIAL_NO_NOT_FOUND_IN_WARRANTY_INFO_TABLE_MSG="Old Serialno not found in warranty info table";
-	public static final String DUAL_SERIAL_NOT_FOUND_MSG="Required both DualSerialNoIn and DualSerialNoOut.";
-	public static final String TRI_SERIAL_NOT_FOUND_MSG ="Required TriSerialNoIn,TriSerialNoOut,DualSerialNoIn and DualSerialNoOut.";
-	public static final String EMAIL_MSG_CODE="8002";
-	public static final String EMAIL_MSG="Error - Contact Support Team.";
-	//R12 scrap serial
+	public static final String OLD_SERIAL_NO_NOT_FOUND_IN_SHIPMENT_TABLE_MSG = "Old Serialno not found in Shipment table";
+	public static final String READING_OLD_SERIAL_NO_INTO_NEW_SERIAL_NO = "Reading old serial number data and Inserted into new Serial Number in Shipment table";
+	public static final String OLD_SERIAL_NO_NOT_FOUND_IN_WARRANTY_INFO_TABLE_MSG = "Old Serialno not found in warranty info table";
+	public static final String DUAL_SERIAL_NOT_FOUND_MSG = "Required both DualSerialNoIn and DualSerialNoOut.";
+	public static final String TRI_SERIAL_NOT_FOUND_MSG = "Required TriSerialNoIn,TriSerialNoOut,DualSerialNoIn and DualSerialNoOut.";
+	public static final String EMAIL_MSG_CODE = "8002";
+	public static final String EMAIL_MSG = "Error - Contact Support Team.";
+	// R12 scrap serial
 	public static final String OLD_SERIAL_FOUND_SUCCSS_MSG = "Success";
 	public static final String OLD_SN_SUCCESS = "0000";
-	public static final String R12_OLD_SN_NOT_AVAILABLE= "5015";
+	public static final String R12_OLD_SN_NOT_AVAILABLE = "5015";
 	public static final String R12_SN_NOT_VALID = "5016";
 
 }
