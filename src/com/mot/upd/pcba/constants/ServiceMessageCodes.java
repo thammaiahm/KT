be21d3cca57894180ffa3f7ccafa4d38fed06cbb
Motorola123 <rviswa@motorola.com>
2015-05-09 10:10:34 +0530

    Modified Success case in MEID and IMEI
    Change-Id: I0d13c4a8ddc6295fcd7cb3433c1040a506128a81
    Reviewed-on: http://gerrit.mot.com/746249
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Haruit Kumar <hkumar@motorola.com>

--- src/com/mot/upd/pcba/constants/ServiceMessageCodes.java
+++ src/com/mot/upd/pcba/constants/ServiceMessageCodes.java
@@ -66,14 +66,14 @@ public class ServiceMessageCodes {
 
 	// Viswanath added
 
-	public static final String MEID_SUCCES_MSG = "MEID Success";
+	public static final String MEID_SUCCES_MSG = "Success";
 	public static final String MEID_FAILURE_MSG = "MEID Failure";
 	public static final String INVALID_STATUS_MSG = "Status should be either S or F";
 	public static final String NO_LOCK_CODE_DETAILS_FOUND_MSG = "No Lock Code Details Found";
 	public static final String OLD_SERIAL_NO_NOT_FOUND_MSG = "Old Serial Not Found";
 	public static final String NEW_SERIAL_NO_NOT_FOUND_MSG = "New Serial Not Found";
 	public static final String NO_DATASOURCE_FOUND_FOR_SERIAL_NO_MSG = "No DataSource found for SerialNO.";
-	public static final String IMEI_SUCCES_MSG = "IMEI Success";
+	public static final String IMEI_SUCCES_MSG = "Success";
 	public static final String IMEI_FAILURE_MSG = "IMEI Failure";
 	public static final String PCBA_INPUT_PARAM_MISSING_MSG = "Following Fields are mandatory- Serial Number,rsdID,mascID,status,snType and either one is mandatory msl,otksl and servicePassCode.Please re-enter and try again.";
 	public static final String PCBA_INPUT_PARAM_MISSING = "Following Fields are mandatory- ClientIP,mascID,serialNoIn,serialNoOut,serialNoType,repairdate.Please re-enter and try again.";
