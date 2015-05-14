fbe8aa55ff7f537a0c145fba614d85568007c91f
Thammaiah <hrdj36@motorola.com>
2015-05-14 13:02:11 +0530

    Added to fix lock code issue
    Change-Id: I9c6c617a95a60782659f5fcb0bfa63fcadaf1335
    Reviewed-on: http://gerrit.mot.com/747786
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Haruit Kumar <hkumar@motorola.com>

---
+++ src/com/mot/upd/pcba/pojo/DispatchSerialMEID.java
@@ -0,0 +1,48 @@
+package com.mot.upd.pcba.pojo;
+
+public class DispatchSerialMEID {
+	
+	private static String aKey1Type;
+	private static String aKey1Value;
+	private static String aKey2Type;
+	private static String aKey2Value;
+	private static String masterSubLockCode;
+	private static String oneTimeSublockCode;
+	public static String getaKey1Type() {
+		return aKey1Type;
+	}
+	public static void setaKey1Type(String aKey1Type) {
+		DispatchSerialMEID.aKey1Type = aKey1Type;
+	}
+	public static String getaKey1Value() {
+		return aKey1Value;
+	}
+	public static void setaKey1Value(String aKey1Value) {
+		DispatchSerialMEID.aKey1Value = aKey1Value;
+	}
+	public static String getaKey2Type() {
+		return aKey2Type;
+	}
+	public static void setaKey2Type(String aKey2Type) {
+		DispatchSerialMEID.aKey2Type = aKey2Type;
+	}
+	public static String getaKey2Value() {
+		return aKey2Value;
+	}
+	public static void setaKey2Value(String aKey2Value) {
+		DispatchSerialMEID.aKey2Value = aKey2Value;
+	}
+	public static String getMasterSubLockCode() {
+		return masterSubLockCode;
+	}
+	public static void setMasterSubLockCode(String masterSubLockCode) {
+		DispatchSerialMEID.masterSubLockCode = masterSubLockCode;
+	}
+	public static String getOneTimeSublockCode() {
+		return oneTimeSublockCode;
+	}
+	public static void setOneTimeSublockCode(String oneTimeSublockCode) {
+		DispatchSerialMEID.oneTimeSublockCode = oneTimeSublockCode;
+	}
+
+}
